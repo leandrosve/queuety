d9723e2c35fb65965923137e88b787f7a59891c8
leandrosve <leandrosvetlich@gmail.com>
2023-10-12 14:18:44 -0300

    Extract allowed origins to env

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,6 +1,7 @@
 const getAllowedOrigins = () => {
   const allowedOrigins = process.env.ALLOWED_URLS ?? '';
   const origins = allowedOrigins.split(',');
+  console.log("ORIGINSSSS", {origins})
   return origins;
 };
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import allowedOrigins from './config/allowedOrigins';
+import getAllowedOrigins from './config/allowedOrigins';
 
 async function bootstrap() {
   const config = new DocumentBuilder().setTitle('Queuety API Docs').setVersion('1.0').addTag('queuety').build();
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: allowedOrigins });
+  app.enableCors({ origin: getAllowedOrigins() });
   app.setGlobalPrefix('/api');
 
   app.useGlobalPipes(
